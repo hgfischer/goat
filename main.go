64b41d7d556dc4359b2723a46c1bc5831d3ab86e
Herbert <herbert.fischer@gmail.com>
2017-01-06 18:49:13 +0100

    Testing the go scheduler

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+import "runtime"
+import "time"
+
+func cpuIntensive(p *int) {
+	for i := 1; i <= 100000000000; i++ {
+		*p = i
+		runtime.Gosched()
+	}
+}
+
+func main() {
+	runtime.GOMAXPROCS(1)
+
+	x := 0
+	go cpuIntensive(&x)
+
+	time.Sleep(100 * time.Millisecond)
+
+	// printed only after cpuIntensive is completely finished
+	fmt.Printf("x = %d.\n", x)
+}
