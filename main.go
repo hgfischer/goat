74d21295d0368a26096adc66c70f3b2bdc87221c
Herbert <herbert.fischer@gmail.com>
2017-01-26 00:09:19 +0100

    Add basic initial test with Serf

--- main.go
+++ main.go
@@ -1,24 +1,63 @@
 package main
 
-import "fmt"
-import "runtime"
-import "time"
+import (
+	"fmt"
+	"log"
 
-func cpuIntensive(p *int) {
-	for i := 1; i <= 100000000000; i++ {
-		*p = i
-		runtime.Gosched()
+	"os"
+
+	"github.com/hashicorp/serf/serf"
+)
+
+var (
+	nodes = []string{
+		"10.20.30.41", "10.20.30.42", "10.20.30.43",
+	}
+
+	serfReconcile = make(chan serf.Member, 32)
+	serfEvents    = make(chan serf.Event, 128)
+)
+
+func main() {
+	conf := serf.DefaultConfig()
+	conf.Init()
+	conf.MemberlistConfig.BindAddr = os.Args[1]
+	conf.MemberlistConfig.BindPort = 12345
+	conf.MemberlistConfig.AdvertiseAddr = conf.MemberlistConfig.BindAddr
+	conf.MemberlistConfig.AdvertisePort = conf.MemberlistConfig.BindPort
+	conf.EventCh = serfEvents
+	conf.Tags["role"] = "goat"
+	fmt.Printf("%#v\n", conf)
+
+	s, err := serf.Create(conf)
+	fatalError(err)
+
+	fmt.Printf("%#v\n", s.Memberlist())
+
+	if len(os.Args) > 2 {
+		nNodes, err := s.Join(os.Args[2:], true)
+		fatalError(err)
+		log.Println("Nodes:", nNodes)
+	}
+
+	for ev := range serfEvents {
+		switch ev.EventType() {
+		case serf.EventMemberJoin:
+			fmt.Printf("Member Join: %#v\n", ev)
+		case serf.EventMemberFailed:
+			fmt.Printf("Member Failed: %#v\n", ev)
+		case serf.EventMemberLeave:
+			fmt.Printf("Member Leave: %#v\n", ev)
+		case serf.EventMemberReap:
+			fmt.Printf("Member Reap: %#v\n", ev)
+		case serf.EventMemberUpdate:
+			fmt.Printf("Member Update: %#v\n", ev)
+		}
 	}
 }
 
-func main() {
-	runtime.GOMAXPROCS(1)
-
-	x := 0
-	go cpuIntensive(&x)
-
-	time.Sleep(100 * time.Millisecond)
-
-	// printed only after cpuIntensive is completely finished
-	fmt.Printf("x = %d.\n", x)
+func fatalError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
